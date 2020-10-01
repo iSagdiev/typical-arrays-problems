bb05205c65b4c3763a23d08e76082e5ac6fc9c0b
iSagdiev <jay.iskander@gmail.com>
2020-10-01 21:38:06 +0500

    init: Made function for the current task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
 
 exports.min = function min (array) {
-  return 0;
+    if ( typeof array === 'undefined' || isNaN(array[0])) {return 0 }
+    else {
+        array.sort((a,b) => a - b)
+        return array[0]
+    }
 }
 
 exports.max = function max (array) {
-  return 0;
+    if ( typeof array === 'undefined' || isNaN(array[0])) {return 0 }
+    else {
+        array.sort((a,b) => a - b)
+        return array[array.length-1]
+    }
+  
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg (array) {    
+    if ( typeof array === 'undefined' || isNaN(array[0])) {return 0 }
+    else {
+    let total = 0;
+    for(let i = 0; i < array.length; i++) {
+      total += array[i];
+    }
+    return total/array.length;
+  }
 }
